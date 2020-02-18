6a189b984968ff5e593552ea36f9886d1ea4cf2f
kenyeres.patrik <kenyeres.patrik@C41-14>
2020-02-18 13:45:15 +0100

    Kiíró elkészült

--- Biliárd git/Program.cs
+++ Biliárd git/Program.cs
@@ -12,13 +12,33 @@ namespace Biliárd_git
         {
             List<int> golyók = new List<int>();
             Kezdés(golyók);
-            for (int i = 0; i < golyók.Count; i++)
-            {
-                Console.WriteLine(golyók[i]);
-            }
+            int leeső = leesősorsoló(golyók);
+            Kiíró(golyók);
+
+            //for (int i = 0; i < golyók.Count; i++)
+            //{
+            //    Console.WriteLine(golyók[i]);
+            //}
 
 
             Console.ReadKey();
+        }
+
+        private static void Kiíró(List<int> golyók)
+        {
+            foreach (int item in golyók)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        private static int leesősorsoló(List<int> golyók)
+        {
+            Random gép = new Random();
+            int szám = gép.Next(golyók.Count);
+            golyók.Remove(szám);
+            return golyók[szám];
+
         }
 
         private static void Kezdés(List<int> golyók)
